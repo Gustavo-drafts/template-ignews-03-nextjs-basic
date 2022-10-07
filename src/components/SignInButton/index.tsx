31d4266aa8f41dacda6ed53f2ec17bc998e8b7da
gutoo01 <gu.trindadelive@gmail.com>
2022-10-07 20:44:05 -0300

    fluxo de autenticação JWT next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,13 +1,35 @@
-import { FaGithub } from 'react-icons/fa'
-import styles from './styles.module.scss'
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import styles from './styles.module.scss';
+
+
 
 export function SignInButton() {
-  return (
-    <button 
-    type="button"
-    className={styles.SignInButton}
+  const { data: session } = useSession()
+
+  console.log(session)
+
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.SignInButton}
+      onClick={() => signOut()}
     >
-      <FaGithub />
+      <FaGithub color="#04d361" />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+
+  ) : (
+
+    <button
+      type="button"
+      className={styles.SignInButton}
+      onClick={() => signIn('github')}
+    >
+      <FaGithub color="#eba417" />
       Sign in with Github
     </button>
   )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
-import { AppProps } from "next/app"
+import { SessionProvider } from "next-auth/react";
 import Header from "../components/Header";
 
 import '../styles/global.scss';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
