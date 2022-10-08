c7d83a6722eff93f9c96cfe86eb8a198425b7f11
gutoo01 <gu.trindadelive@gmail.com>
2022-10-08 20:01:45 -0300

    salvando usuário no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,10 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from 'faunadb';
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { fauna } from '../../../services/fauna';
+
+
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -10,6 +15,25 @@ export const authOptions = {
       authorization: { params: { scope: "read:user" } }
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create( //inserir dados no banco
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true
+
+      } catch {
+        return false
+      }
+    }
+  },
 }
 
 export default NextAuth(authOptions)
