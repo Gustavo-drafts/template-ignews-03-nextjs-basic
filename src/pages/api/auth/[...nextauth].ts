ae588fcea41e69e788d99361f18b09f3b32d9e5b
gutoo01 <gu.trindadelive@gmail.com>
2022-10-15 00:34:15 -0300

    redirect client após clicar em Subscribe (Checkout Session)

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,11 +21,29 @@ export const authOptions = {
 
       try {
         await fauna.query(
-          q.Create( //inserir dados no banco
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
+        console.log(user)
+        
 
         return true
 
